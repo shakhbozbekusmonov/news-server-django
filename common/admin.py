306be538e5226e6ac9fc9bf64fc2d59ab46f3690
shakhbozbekusmonov <miracle_0319@bk.ru>
2024-01-06 09:32:26 +0500

    Jazzmin Admin panel configurated

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from news.models import News, Category
+
+
+admin.site.register(Category)
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    prepopulated_fields = {"slug": ('title', )}
+
+
+admin.site.site_header = 'News Admin'
+admin.site.site_title = 'News site admin'
